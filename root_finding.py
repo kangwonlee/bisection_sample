6ada5c41af480cd8a0d9d22aae69d43e62143565
KangWon LEE <kangwon.lee.edu@gmail.com>
2020-10-28 03:34:18 +0000

    removed py.arange

--- root_finding.py
+++ root_finding.py
@@ -1,26 +1,26 @@
-import pylab as py
-
-
 def sequential(f, x_init, x_interval, epsilon, x_final):
     # result 의 초기값
     # Initial value for sqrt_10
     result = 'Not Found'
 
+    # i+1 번째 x 와 i 번째 x 의 간격이 너무 작지는 않은지 확인
+    # check if the interval between i+1st x and i-th x is too small
+    assert epsilon < abs(x_interval), x_interval
+
     # 일련의 x_i 값을 미리 준비한다
     # Prepare a series of x_i values in advance
-    x_array = py.arange(x_init, x_final+x_interval*0.5, x_interval)
+    n = int((x_final - x_init) / x_interval) + 1
 
-    # 몇번 반복했는지 측정해 보자
-    # Let's count the number of iterations
-    counter = 0
+    # i 에 관한 반복문
+    # i loop
+    for i in range(n):
+        # i 번째 x
+        # i-th x
+        x_i = x_init + x_interval * i
 
-    # x_i 에 관한 반복문
-    # x_i loop
-    for x_i in x_array:
         # Evaluate the function
         y_i = f(x_i)
         
-        counter += 1
         # Check if absolute value is smaller than epsilon
         if abs(y_i) < epsilon:
             result = x_i
@@ -29,7 +29,7 @@ def sequential(f, x_init, x_interval, epsilon, x_final):
 
     # 반복 횟수
     # Number of iterations
-    print('counter =', counter)
+    print('counter =', i)
 
     return result
 
